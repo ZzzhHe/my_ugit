c345bdd95539514a5e44cdb30f7ed10d3db08181
ZhuohaoHe <zhuohao.he@outlook.com>
2023-02-07 13:38:52 +0800

    hash-object: Save object

--- ugit/cli.py
+++ ugit/cli.py
@@ -26,10 +26,24 @@ def parse_args():
     # create parese for 'init' command, and bind 'init' with init()
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
+    
+    # create parese for 'hash-object' command, and bind 'hash-object' with hash_object()
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
 
     # Namespace(command='init') 
+    # Namespace(command='hash-object', argument='file') 
     return parser.parse_args()
     
 def init(args):
+    """ init a empty ugit respository
+    """
     data.init()
     print(f'Initialized empty ugit respository in {os.getcwd()}/{data.GIT_DIR}')
+    
+def hash_object(args):
+    """ refer to the file's object using its hash
+    """
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
--- ugit/data.py
+++ ugit/data.py
@@ -2,10 +2,21 @@
 Manages the data in .ugit directory. 
 Here will be the code that actually touches files on disk.
 """
-
 import os
+import hashlib
 
 GIT_DIR = ".ugit"
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
+    
+def hash_object(data):
+    """ 
+    refer to the file's object using its hash (content-addressable storage)
+    and create a new oid path in 'objects'
+    """
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
