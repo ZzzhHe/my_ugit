2542c1d079466d0fc38e8f8fc8a8df922ab96b01
ZhuohaoHe <zhuohao.he@outlook.com>
2023-02-07 12:20:15 +0800

    Add argument parser in cli.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
+"""使用 setup.py 来构建自己的 ugit 包"""
+
 #!/usr/bin/env python3
 
+"""
+在 Python 中，setup.py 是用于构建和分发 Python 包的模块。
+它通常包含有关包的信息，例如名称、版本和依赖项，以及构建和安装包的说明。
+
+为了能够在开发的同时运行ugit包来检测效果，需要使用开发模式
+python3 setup.py develop --user
+"""
+
+# https://setuptools.pypa.io/en/latest/userguide/quickstart.html
 from setuptools import setup
 
 setup (name = 'ugit',
-       version = '1.0',
-       packages = ['ugit'],
-       entry_points = {
-           'console_scripts' : [
-               'ugit = ugit.cli:main'
-           ]
-       })
+        version = '1.0',
+        packages = ['ugit'],
+        entry_points = {
+            'console_scripts' : [
+                'ugit = ugit.cli:main'
+            ]
+        })
--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,30 @@
+"""
+Add argument parser
+"""
+
+# https://docs.python.org/3/library/argparse.html
+import argparse
+
 def main():
+    # parse the args and call whatever function was selected
+    args = parse_args()
+    args.func(args)
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
+    
+    # when a program performs several different functions 
+    # which require different kinds of command-line arguments
+    # use subparsers
+    commands = parser.add_subparsers(dest="command")
+    commands.required = True
+    
+    # create parese for 'init' command, and bind 'init' with init()
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    # Namespace(command='init') 
+    return parser.parse_args()
+    
+def init(args):
     print("Hello World")
