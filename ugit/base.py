a3bb74b704a3b4142d759a4f403f4262961d5af7
ZhuohaoHe <zhuohao.he@outlook.com>
2023-02-25 17:05:14 +0800

    add: Allow adding a directory

--- ugit/base.py
+++ ugit/base.py
@@ -452,15 +452,36 @@ def get_oid(name):
 
 def add(filenames):
     """
-    return index, which is a dictionary that maps filenames to their last remembered OID
+    update index, which is a dictionary that maps filenames to their last remembered OID
     """
+    def add_file(filename):
+        """
+        handle file path
+        """
+        # Normalize path
+        filename = os.path.relpath(filename)
+        with open(filename, 'rb') as f:
+            oid = data.hash_object(f.read())
+        index[filename] = oid
+    
+    def add_directory(dirname):
+        """
+        handle directory path
+        """
+        for root, _, filenames in os.walk(dirname):
+            for filename in filenames:
+                path = os.path.relpath(f'{root}/{filename}')
+                if is_ignored(path) or not os.path.isfile(path):
+                    continue
+                add_file(path)
+    
     with data.get_index() as index:
-        for filename in filenames:
-            # Normalize path
-            filename = os.path.relpath(filename)
-            with open(filename, 'rb') as f:
-                oid = data.hash_object(f.read())
-            index[filename] = oid
+        for name in filenames:
+            if os.path.isfile(name):
+                add_file(name)
+            elif os.path.isdir(name):
+                add_directory(name)
+        
 
 def is_ignored(path):
     """
